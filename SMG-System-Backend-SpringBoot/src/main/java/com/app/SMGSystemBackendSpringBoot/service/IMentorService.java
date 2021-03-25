489d02a8dadc110b9dcb36fd38bd805766eb3558
mukulmc03 <mukulchikhalkar.mc@gmail.com>
2021-03-25 13:30:43 +0530

    Renamed Project!

---
+++ SMG-System-Backend-SpringBoot/src/main/java/com/app/SMGSystemBackendSpringBoot/service/IMentorService.java
@@ -0,0 +1,29 @@
+package com.app.SMGSystemBackendSpringBoot.service;
+
+import java.util.List;
+
+import com.app.SMGSystemBackendSpringBoot.dto.MentorAddressDTO;
+import com.app.SMGSystemBackendSpringBoot.pojo.Address;
+import com.app.SMGSystemBackendSpringBoot.pojo.Mentor; 
+
+public interface IMentorService {
+	//to get list of students assigned to mentorId
+	public List<?> getAssignedStudents(int mentorId);
+
+	//to update marks of student
+	public String updateMarksOfStudent(int studentId, float studentsMarks);
+
+	//upate mentor
+	public Mentor updateMentor(int mentorId, MentorAddressDTO mentorAddressDto);
+
+	//to get mentor by id
+	public Mentor getMentorById(int mentorId);
+
+	//to delete mentor
+	public String deleteMentorById(int mentorId);
+
+	//to register mentor with address and course
+	public String registerMentor(int courseId, MentorAddressDTO mentorAddressDto);
+
+	Address getAddressByMentorId(int mentorId);
+}
